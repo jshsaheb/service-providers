40839368e4749f2b88ef0feb1a79f14a2b7dbf90
shahul <jshsaheb@gmail.com>
2020-08-04 23:04:54 +0200

    Updates

---
+++ precache-manifest.aa53cbfd7c7726cddcf9618936867c6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62183e70111b4c420b13f39d183b1a7",
+    "url": "/service-providers/index.html"
+  },
+  {
+    "revision": "440b5e5ac3082415d3d0",
+    "url": "/service-providers/static/css/2.23a9c941.chunk.css"
+  },
+  {
+    "revision": "7508c938247b26cfba80",
+    "url": "/service-providers/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "440b5e5ac3082415d3d0",
+    "url": "/service-providers/static/js/2.f4a7e488.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/service-providers/static/js/2.f4a7e488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7508c938247b26cfba80",
+    "url": "/service-providers/static/js/main.18ce0b42.chunk.js"
+  },
+  {
+    "revision": "92dc2681a98f24653d2f",
+    "url": "/service-providers/static/js/runtime-main.8f51ac7a.js"
+  }
+]);
